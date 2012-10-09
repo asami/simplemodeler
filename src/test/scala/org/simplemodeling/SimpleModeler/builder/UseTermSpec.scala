2787b6c74d3cc8a5378d9ee8acd83fa01df0ba62
ASAMI Tomoharu <asami.tomoharu@gmail.com>
2012-10-10 06:03:47 +0900

    Adds multiplicity test in UseTermSpec.

--- src/test/scala/org/simplemodeling/SimpleModeler/builder/UseTermSpec.scala
+++ src/test/scala/org/simplemodeling/SimpleModeler/builder/UseTermSpec.scala
@@ -5,10 +5,11 @@ import org.scalatest.junit.JUnitRunner
 import org.scalatest.{WordSpec, GivenWhenThen}
 import org.scalatest.matchers.ShouldMatchers
 import org.simplemodeling.SimpleModeler._
+import org.simplemodeling.SimpleModeler.entities.simplemodel._
 
 /**
  * @since   Sep. 17, 2012
- * @version Sep. 17, 2012
+ * @version Oct. 10, 2012
  * @author  ASAMI, Tomoharu
  */
 @RunWith(classOf[JUnitRunner])
@@ -25,15 +26,21 @@ class UseTermSpec extends WordSpec with ShouldMatchers with GivenWhenThen with U
         get_type_name_explicitly("abc:xyz*") should be (Some("xyz"))
       }
     }
-    "get_type_name_explicitly" that {
-      "no type" in {
-        get_type_name_explicitly("abc") should be (None)
+    "get_multiplicity_by_term" that {
+      "implicit one" in {
+        get_multiplicity_by_term("abc") should be (GROne)
       }
-      "explicitly type" in {
-        get_type_name_explicitly("abc:xyz") should be (Some("xyz"))
+      "type and implicit one" in {
+        get_multiplicity_by_term("abc:xyz") should be (GROne)
       }
-      "explicitly type with zeroMore" in {
-        get_type_name_explicitly("abc:xyz*") should be (Some("xyz"))
+      "zero-one" in {
+        get_multiplicity_by_term("abc:xyz?") should be (GRZeroOne)
+      }
+      "one-more" in {
+        get_multiplicity_by_term("abc:xyz+") should be (GROneMore)
+      }
+      "zero-more" in {
+        get_multiplicity_by_term("abc:xyz*") should be (GRZeroMore)
       }
     }
   }
