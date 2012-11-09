f458bdd4e4fcf1d36517bf5ee3e1eb6eaf893956
ASAMI Tomoharu <asami.tomoharu@gmail.com>
2012-11-09 11:27:05 +0900

    Fixes powertype kinds in java.

--- src/main/scala/org/simplemodeling/SimpleModeler/entities/PowertypeJavaClassDefinition.scala
+++ src/main/scala/org/simplemodeling/SimpleModeler/entities/PowertypeJavaClassDefinition.scala
@@ -5,7 +5,7 @@ import org.simplemodeling.SimpleModeler.entity.SMPowertype
 /*
  * @since   Feb. 20, 2012
  *  version Feb. 20, 2012
- * @version Nov.  8, 2012
+ * @version Nov.  9, 2012
  * @author  ASAMI, Tomoharu
  */
 class PowertypeJavaClassDefinition(
@@ -33,33 +33,13 @@ class PowertypeJavaClassDefinition(
   override protected def attribute_variables_Prologue {
     if (pobject.hasInheritance) return
     val mpower = pobject.modelObject.asInstanceOf[SMPowertype]
+    val kinds = mpower.kinds
     val labels =
-      if (mpower.isEmpty) List("Unkonwn")
-      else mpower.kinds.map(_.name)
+      if (kinds.isEmpty) List("Unkonwn")
+      else kinds.map(_.name)
     jm_pln(labels.mkString("", ", ", ";"))
   }
-/*
-  protected def attribute_variables_Prologue0 {
-    val mpower = pobject.modelObject.asInstanceOf[SMPowertype]
-    val last = mpower.kinds.last
-    for (k <- mpower.kinds) {
-      val key = k.name
-      val label = k.term
-      jm_p("""%s("%s", "%s")""".format(key, key, label))
-      if (k eq last) {
-        jm_pln(";")
-      } else {
-        jm_pln(",")
-      }
-    }
-  }
-*/
 
-//  override protected def attribute(attr: PAttribute) = {
-//    new ValueJavaClassAttributeDefinition(pContext, aspects, attr, this, jm_maker)
-//  }
-
-//  override protected def to_methods_string {}
   override protected def object_methods_hashcode {}
   override protected def object_methods_equals {}
 }
